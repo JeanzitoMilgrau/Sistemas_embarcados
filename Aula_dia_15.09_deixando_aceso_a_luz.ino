5df8f883c76030b1166bf2386aeef34fa63ab608
JeanzitoMilgrau <141688238+JeanzitoMilgrau@users.noreply.github.com>
2023-09-15 22:30:08 -0300

    Ligando um botão desligado o outro

--- Aula_dia_15.09_deixando_aceso_a_luz.ino
+++ Aula_dia_15.09_deixando_aceso_a_luz.ino
@@ -1,9 +1,13 @@
 int vAtu=0;
 int vAnt=0;
 int ligado=0;
+int religado=0;
 void setup() {
   pinMode(2, INPUT);
   pinMode(13,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode(11,OUTPUT);
+  
 }
 void loop (){
   vAnt = vAtu;
@@ -12,7 +16,13 @@ void loop (){
   if ((vAtu==1)and (vAnt==0))
   {
    ligado = not ligado;
+   if (ligado == religado){
+   religado=not religado;
+   digitalWrite(12, religado); 
+   }
+   
+   
   }
-   digitalWrite(13, ligado);  
-  
+   digitalWrite(13, ligado); 
+   digitalWrite(11, ligado);
 }
